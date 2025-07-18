d34926cc30b46c3412daba8d264cfab634a8f4fd
root <root@gvkpuetjdb.local>
2025-07-18 12:27:41 +0000

    Удалены старые файлы

--- yandex_anable.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# Простой тестовый запрос к API
-url = "https://api-metrika.yandex.net/management/v1/counters"
-headers = {"Authorization": "OAuth YOUR_TOKEN"}
-response = requests.get(url, headers=headers)
-print(response.status_code, response.json())
