2a57dba740b459abe8d5d704ba97a341742cc076
Viktor Ivanov <49984907+ivo666@users.noreply.github.com>
2025-08-07 16:10:11 +0300

    Update rdl_to_cdm_tables_update.py

--- rdl_to_cdm_tables_update.py
+++ rdl_to_cdm_tables_update.py
@@ -89,7 +89,7 @@ def get_queries():
                          	when t.traffic_source  = 'social' then social_network
                          	when t.traffic_source  = 'messenger' then t.messenger 
                          	when t.traffic_source  = 'direct' then 'direct'
-                         	when t.traffic_source = 'internal' THEN regexp_replace(t.referer, '^https?://([^/]+).*$', '\1')
+                         	when t.traffic_source = 'internal' THEN regexp_replace(convert_from(t.referer::bytea, 'UTF-8'), '^https?://([^/]+).*$', '\1')
                           else 'indef'
                          END as source
                         , case
